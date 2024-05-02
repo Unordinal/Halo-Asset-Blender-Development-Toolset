d562f509e0ef2a58863ebe52864d7c9880f9edca
Unordinal <unordinal@gmail.com>
2024-05-01 22:01:10 -0700

    Work on ensuring H1 overlay animation import -> export parity.
    The goal is to be able to import a .jmo, be able to easily modify it in Blender, and then export it and have it work in-game.
    
    An imported -> directly exported .jmo should ideally have the exact same resulting animation in-game, though the output file will most likely never match the input due to differences in floating-point precision and such.
    
    This current code *works* for a given definition of the word, though it's expansive, wordy, and brittle, with bad perf. The singular animation I've tested so far is the Elite 'stand pistol aim-still' overlay animation, which seemingly exports perfectly and appears identical in-game.
    
    More work to be done.

--- io_scene_halo/file_jma/process_scene.py
+++ io_scene_halo/file_jma/process_scene.py
@@ -25,6 +25,8 @@
 # ##### END MIT LICENSE BLOCK #####
 
 import bpy
+import math
+from mathutils import Matrix, Quaternion, Vector
 
 from .format import JMAAsset
 from ..global_functions import mesh_processing, global_functions, resource_management
@@ -45,6 +47,129 @@ def find_valid_armature(context, obj):
         mesh_processing.select_object(context, obj)
 
     return node_list, valid_armature
+
+def vec_approx(a: Vector, b: Vector, rel_tol=1e-05):
+    return math.isclose(a.x, b.x, rel_tol=rel_tol) and \
+        math.isclose(a.y, b.y, rel_tol=rel_tol) and \
+        math.isclose(a.z, b.z, rel_tol=rel_tol)
+
+def quat_approx(a: Quaternion, b: Quaternion, rel_tol=1e-05):
+    return math.isclose(a.x, b.x, rel_tol=rel_tol) and \
+        math.isclose(a.y, b.y, rel_tol=rel_tol) and \
+        math.isclose(a.z, b.z, rel_tol=rel_tol) and \
+        math.isclose(a.w, b.w, rel_tol=rel_tol)
+
+def get_jmo_matrix(node, *, invert_rot=True, local=False) -> Matrix:
+    result_matrix = node.matrix_basis
+    if not local:
+        parent_matrix = node.parent.matrix if node.parent else Matrix.Translation(node.bone.head)
+        result_matrix = parent_matrix.inverted() @ node.matrix
+
+    loc = result_matrix.to_translation()
+    rot = result_matrix.to_quaternion()
+    scale = result_matrix.to_scale()
+    if invert_rot:
+        rot = rot.inverted()
+    return Matrix.LocRotScale(loc, rot, scale)
+
+def matrix_trs_separate_equals(a: Matrix, b: Matrix, *, rel_tol=1e-05):
+    """
+    Compare the separate TRS components of two matrices, returning a tuple indicating the equality of each
+    respective component in the order Translation, Rotation, Scale.
+    """
+    a_loc, a_rot, a_scl = a.decompose()
+    b_loc, b_rot, b_scl = b.decompose()
+    f_loc = vec_approx(a_loc, b_loc, rel_tol=rel_tol)
+    f_rot = quat_approx(a_rot, b_rot, rel_tol=rel_tol)
+    f_scl = vec_approx(a_scl, b_scl, rel_tol=rel_tol)
+    return (f_loc, f_rot, f_scl)
+
+def trs_flag_tuple_and(a: tuple[bool, bool, bool], b: tuple[bool, bool, bool]):
+    return (a[0] and b[0], a[1] and b[1], a[2] and b[2])
+
+def create_overlay_flags_list(context, node_list, base_anim_xforms, frame_range):
+    flags_list: list[tuple[bool, bool, bool]] = [(True, True, True)] * len(node_list)
+    last_matrices: list[Matrix] = [None] * len(node_list)
+    for frame in frame_range:
+        context.scene.frame_set(frame)
+        for node_idx, node in enumerate(node_list):
+            if all(f == False for f in flags_list[node_idx]): # Flags already determined for this node.
+                continue
+
+            curr_matrix = get_jmo_matrix(node)
+            last_matrix = last_matrices[node_idx]
+            if last_matrix:
+                flags_list[node_idx] = trs_flag_tuple_and(flags_list[node_idx], matrix_trs_separate_equals(curr_matrix, last_matrix))
+
+            base_matrix = base_anim_xforms.get(node.name)
+            if base_matrix:
+                flags_list[node_idx] = trs_flag_tuple_and(flags_list[node_idx], matrix_trs_separate_equals(curr_matrix, base_matrix))
+            else:
+                print("WARNING: No base animation transform found for node '%s', assuming default." % node.name)
+            
+            last_matrices[node_idx] = curr_matrix
+    return flags_list
+
+def find_base_anim(action_name):
+    split = action_name.split()
+    if len(split) == 3:
+        if split[2].startswith("aim-still"):
+            search_start = " ".join(split[:2]) + " idle"
+            found = next((a for a in bpy.data.actions if a.name.startswith(search_start)), None)
+            if not found:
+                search_start = split[0] + " idle"
+                found = next((a for a in bpy.data.actions if a.name.startswith(search_start)), None)
+            return found
+        if split[2].startswith("aim-move"):
+            search_start = " ".join(split[:2]) + " move-front"
+            found = next((a for a in bpy.data.actions if a.name.startswith(search_start)), None)
+            if not found:
+                search_start = split[0] + " move-front"
+                found = next((a for a in bpy.data.actions if a.name.startswith(search_start)), None)
+            return found
+    return None
+
+def get_base_anim_transforms(context, armature):
+    prev_action = armature.animation_data.action
+    base_action = find_base_anim(prev_action.name) #bpy.data.actions["stand pistol idle%0"]
+    if not base_action:
+        return dict()
+        
+    armature.animation_data.action = base_action
+    context.scene.frame_set(1)
+    #context.view_layer.update()
+    base_transforms = {n.name: get_jmo_matrix(n) for n in armature.pose.bones}
+    armature.animation_data.action = prev_action
+    return base_transforms
+
+def get_jmo_transforms(context, armature, node_list, frame_range, invert_rot=True):
+    base_anim_xforms = get_base_anim_transforms(context, armature)
+    default_frame_flags_list = create_overlay_flags_list(context, node_list, base_anim_xforms, frame_range)
+    
+    transforms = []
+    for frame in frame_range:
+        context.scene.frame_set(frame)
+        transforms_for_frame = []
+        for node_idx, node in enumerate(node_list):
+            flags = default_frame_flags_list[node_idx]
+            node_matrix = get_jmo_matrix(node, invert_rot=invert_rot)
+            loc = node_matrix.to_translation()
+            rot = node_matrix.to_quaternion() if flags[1] else get_jmo_matrix(node, invert_rot=invert_rot, local=True).to_quaternion()
+            scale = node_matrix.to_scale()
+
+            translation = (loc[0], loc[1], loc[2])
+            rotation = (rot[1], rot[2], rot[3], rot[0])
+            scale = (scale[0])
+            transforms_for_frame.append(JMAAsset.Transform(translation, rotation, scale))
+        transforms.append(transforms_for_frame)
+    
+    # Set overlay default transform
+    context.scene.frame_set(frame_range.start)
+    for node_idx, node in enumerate(node_list):
+        flags = default_frame_flags_list[node_idx]
+        if not flags[1]:
+            transforms[0][node_idx].rotation = Quaternion((0, 0, 0, 1))
+    return transforms
 
 def process_scene(context, extension, jma_version, game_title, generate_checksum, fix_rotations, use_maya_sorting, scale_value):
     JMA = JMAAsset()
@@ -153,23 +278,28 @@ def process_scene(context, extension, jma_version, game_title, generate_checksum
     if generate_checksum:
         JMA.node_checksum = global_functions.node_hierarchy_checksum(JMA.nodes, JMA.nodes[0], JMA.node_checksum)
 
-    for frame in range(first_frame, last_frame):
-        transforms_for_frame = []
-        for node in joined_list:
-            context.scene.frame_set(frame)
-            is_bone = False
-            if armature:
-                is_bone = True
+    use_experimental_overlay_logic = True
+    if use_experimental_overlay_logic and jma_version == 16392: # and extension == "jmo" ?
+        pose_bones = [armature.pose.bones[b.name] for b in joined_list]
+        JMA.transforms = get_jmo_transforms(context, armature, pose_bones, range(first_frame, last_frame))
+    else:
+        for frame in range(first_frame, last_frame):
+            transforms_for_frame = []
+            for node in joined_list:
+                context.scene.frame_set(frame)
+                is_bone = False
+                if armature:
+                    is_bone = True
 
-            bone_matrix = global_functions.get_matrix(node, node, True, armature, joined_list, True, jma_version, 'JMA', False, scale_value, fix_rotations)
-            mesh_dimensions = global_functions.get_dimensions(bone_matrix, node, jma_version, is_bone, 'JMA', scale_value)
-            rotation = (mesh_dimensions.quaternion[0], mesh_dimensions.quaternion[1], mesh_dimensions.quaternion[2], mesh_dimensions.quaternion[3])
-            translation = (mesh_dimensions.position[0], mesh_dimensions.position[1], mesh_dimensions.position[2])
-            scale = (mesh_dimensions.scale[0])
+                bone_matrix = global_functions.get_matrix(node, node, True, armature, joined_list, True, jma_version, 'JMA', False, scale_value, fix_rotations)
+                mesh_dimensions = global_functions.get_dimensions(bone_matrix, node, jma_version, is_bone, 'JMA', scale_value)
+                rotation = (mesh_dimensions.quaternion[0], mesh_dimensions.quaternion[1], mesh_dimensions.quaternion[2], mesh_dimensions.quaternion[3])
+                translation = (mesh_dimensions.position[0], mesh_dimensions.position[1], mesh_dimensions.position[2])
+                scale = (mesh_dimensions.scale[0])
 
-            transforms_for_frame.append(JMA.Transform(translation, rotation, scale))
+                transforms_for_frame.append(JMA.Transform(translation, rotation, scale))
 
-        JMA.transforms.append(transforms_for_frame)
+            JMA.transforms.append(transforms_for_frame)
 
     armature_transform = False
     if jma_version > 16394 and armature_transform:
